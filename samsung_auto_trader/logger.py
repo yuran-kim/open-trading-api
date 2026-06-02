e4fbbfa555f704c0e8ea254287ffd5610eb6becb
Kim Yuran <reinalan0001@gmail.com>
2026-06-02 03:24:09 +0000

    Finalize KST logging and dry-run trading flow

--- samsung_auto_trader/logger.py
+++ samsung_auto_trader/logger.py
@@ -1,15 +1,29 @@
 import logging
+from datetime import datetime
+from zoneinfo import ZoneInfo
 from typing import Optional
+
+class KSTFormatter(logging.Formatter):
+    def formatTime(self, record, datefmt=None):
+        dt = datetime.fromtimestamp(
+            record.created,
+            tz=ZoneInfo("Asia/Seoul")
+        )
+
+        if datefmt:
+            return dt.strftime(datefmt + " %Z")
+
+        return dt.strftime("%Y-%m-%d %H:%M:%S %Z")
 
 
 def get_logger(name: Optional[str] = None) -> logging.Logger:
     logger = logging.getLogger(name or "samsung_auto_trader")
     if not logger.handlers:
         handler = logging.StreamHandler()
-        formatter = logging.Formatter(
+        formatter = KSTFormatter(
             "%(asctime)s %(levelname)s %(name)s %(message)s",
             datefmt="%Y-%m-%d %H:%M:%S",
-        )
+)
         handler.setFormatter(formatter)
         logger.addHandler(handler)
     logger.setLevel(logging.INFO)
